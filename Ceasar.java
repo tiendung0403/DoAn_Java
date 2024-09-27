5c47e45ba9e95bddce1c71225ea40911703d17dd
tiendung0403 <dangtiendung722@gmail.com>
2024-09-28 01:09:48 +0700

    thuat toan Ceasar

---
+++ Ceasar.java
@@ -0,0 +1,19 @@
+public class Ceasar {
+    public String enCrytion(String plaintext,int key) {
+        String ciphertext = "";
+        
+        for (char c: plaintext.toCharArray()) {
+            if (Character.isLetter(c)) {             
+                char n = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
+                ciphertext += (char) ((c - n + key) % 26 + n);
+            }
+            else ciphertext += c;
+        }
+
+        return ciphertext;
+    }
+    public String deCrytion(String ciphertext,int key){
+        return enCrytion(ciphertext, 26 - key);
+    }
+}
+
