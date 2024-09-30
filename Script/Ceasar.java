55e9dd14e843bdbf8f5298c5840b956a96c44478
tiendung0403 <dangtiendung722@gmail.com>
2024-09-30 14:45:49 +0700

    update

--- Script/Ceasar.java
+++ Script/Ceasar.java
@@ -1,6 +1,41 @@
 package Script;
 
 public class Ceasar {
+    private int key;
+    private String plaintext;
+    private String ciphertext;
+
+    public Ceasar (){
+        this.key = 0;
+        this.plaintext = "";
+        this.ciphertext = "";
+    }
+    public void setKey(int k){
+        if (k>=0 && k<=25) {
+            this.key = k;
+        }
+        else {
+            System.err.println("key khong dung");
+        }
+    }
+    public int getKey(){
+        return key;
+    }
+//
+    public void setPlaintext(String p){
+        this.plaintext = p;
+    }
+    public String getPlaintext(){
+        return plaintext;
+    }
+    //
+    public void setCiphertext(String c){
+        this.ciphertext = c ;
+    }
+    public String getCiphertext(){
+        return ciphertext;
+    }
+    //
     public String enCrytion(String plaintext,int key) {
         String ciphertext = "";
         
@@ -14,7 +49,9 @@ public class Ceasar {
 
         return ciphertext;
     }
+    //giải mã 
     public String deCrytion(String ciphertext,int key){
+        
         return enCrytion(ciphertext, 26 - key);
     }
 }
