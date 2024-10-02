1fa5b36f78affba60a8ed1bcb8d0b8924887d4c3
tiendung0403 <dangtiendung722@gmail.com>
2024-10-02 16:38:46 +0700

    comment

--- Script/Bangchudon.java
+++ Script/Bangchudon.java
@@ -11,64 +11,74 @@ public class Bangchudon {
     }
 
     public void setPlaintext(String p){
-        this.plaintext = p;
+        // this.plaintext = "";
+        // for(char c : p.toCharArray()){
+        //     if(c>='a'&& c<='z' || c>='A' && c<='Z'){
+        //         this.plaintext +=c;
+        //     }
+        // }
     }
     public String getPlaintext(){
-        return plaintext;
+        return this.plaintext;
     }
     public void setCiphertext(String c){
-        this.ciphertext = c;
+        // this.ciphertext ="";
+        // for(char i: c.toCharArray()){
+        //     if(i>='a'&& i<='z' || i>='A' && i<='Z'){
+        //         this.ciphertext += i;
+        //     }
+        // }
     }
     public String getCiphertext(){
-        return ciphertext;
+        return this.ciphertext;
     }
     public void setKey(String k){
-        this.key = "";
-        for(char c : k.toCharArray()){
-            if(( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' )){
-                this.key += c;
-            }
-        }
+        // this.key = "";
+        // for(char c: k.toCharArray()){
+        //     if(c>='a'&& c<='z' || c>='A' && c<='Z'){
+        //         this.key += c;
+        //     }
+        // }
     }
     public String getKey(){
-        return key;
+        return this.key;
     }
     public String encrypt (String plaintext,String key){
-        ciphertext = "";
-        int keyLen = key.length();
-        int keyIndex = 0;
+        // ciphertext = "";
+        // int keyLen = key.length();
+        // int keyIndex = 0;
 
-        for (char c: plaintext.toCharArray()) {
-            if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-                char n = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
-                char keynew = Character.toUpperCase(key.charAt(keyIndex % keyLen));
-                keynew -= 'A';
-                ciphertext += (char) ((c - n + keynew) % 26 + n);
-                keyIndex++;
-            }
-            else ciphertext += c;
-        }
+        // for (char c: plaintext.toCharArray()) {
+        //     if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        //         char n = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
+        //         char keynew = Character.toUpperCase(key.charAt(keyIndex % keyLen));
+        //         keynew -= 'A';
+        //         ciphertext += (char) ((c - n + keynew) % 26 + n);
+        //         keyIndex++;
+        //     }
+        //     else ciphertext += c;
+        // }
         return ciphertext;
     }
 
 
     public String decrypt (String ciphertext, String key){
-        plaintext = "";
-        int keyLen = key.length();
-        int keyIndex = 0;
+        // plaintext = "";
+        // int keyLen = key.length();
+        // int keyIndex = 0;
 
-        for (char c: ciphertext.toCharArray()) {
-            if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-                char n = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
-                char keynew = Character.toUpperCase(key.charAt(keyIndex % keyLen));
-                keynew -= 'A';
+        // for (char c: ciphertext.toCharArray()) {
+        //     if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        //         char n = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
+        //         char keynew = Character.toUpperCase(key.charAt(keyIndex % keyLen));
+        //         keynew -= 'A';
 
-                plaintext += (char)((c - n - keynew + 26) % 26 + n);
-                keyIndex++;
+        //         plaintext += (char)((c - n - keynew + 26) % 26 + n);
+        //         keyIndex++;
                 
-            }
-            else plaintext += c;
-        }
+        //     }
+        //     else plaintext += c;
+        // }
         return plaintext;
 
     }
