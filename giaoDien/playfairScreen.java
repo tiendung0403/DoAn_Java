b26348d9f651a8276c59e84e9e2244caba0ca784
np384 <np384@DESKTOP-4MV06GF>
2024-10-06 11:25:26 +0700

    Chuyển 2 hàm check lỗi, xóa kí tự can't see sang lớp cha và thêm comment

--- giaoDien/playfairScreen.java
+++ giaoDien/playfairScreen.java
@@ -6,28 +6,38 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import Script.PlayFair;
 
 public class playfairScreen extends subScreen {
 
-	private  JFrame frame;
 	
 	
 	public playfairScreen(doanlaptrinhmahoa mainClass) {
 		super(mainClass); // Truyền lớp chính vào lớp cha
-		frame = new JFrame();
 	}
 
 	@Override
 	protected JPanel Screen() {
 	    JPanel Panel = super.Screen();
 
-	    //set tọa độ hiển thị (x,y,chiều rộng,chiều cao) cho các thành phần 
-	    
+        // Thêm các thành phần mới
+        
+        
+        // Button generate
+        JButton generateBtn = new JButton("Generate Key");
+        JButton startBtn = new JButton("Start");
+        
+        // Tạo lựa chọn mã hóa hoặc giải mã
+        // Tạo mảng các lựa chọn cho JComboBox
+        String[] options = { "Encryption", "Decryption" };
+        
+        // Tạo JComboBox từ mảng các lựa chọn
+        JComboBox<String> choice = new JComboBox<>(options);
+        
+        
+        //set tọa độ hiển thị (x,y,chiều rộng,chiều cao) cho các thành phần 
 	    
 	    // Key & Key generation
         keyText1.setText("Key");
@@ -44,22 +54,6 @@ public class playfairScreen extends subScreen {
         exitButton.setBounds(700,230,80,30);
         
         
-        // Thêm các thành phần mới
-        
-        
-        // Button generate
-        JButton generateBtn = new JButton("Generate Key");
-        JButton startBtn = new JButton("Start");
-        
-        // Tạo lựa chọn mã hóa hoặc giải mã
-        // Tạo mảng các lựa chọn cho JComboBox
-        String[] options = { "Encryption", "Decryption" };
-        
-        // Tạo JComboBox từ mảng các lựa chọn
-        JComboBox<String> choice = new JComboBox<>(options);
-        
-        
-        
         generateBtn.setBounds(630, 85, 130, 25);
         startBtn.setBounds(500, 150, 100, 30);
         choice.setBounds(610,150,100,30);
@@ -71,21 +65,19 @@ public class playfairScreen extends subScreen {
         Panel.add(choice);
         
 
+        // Kiểm tra key và xuất ra key generation
         generateBtn.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String strKeyTextField = removeInvisibleChars(keyE.getText());
+				String strKeyTextField = removeInvisibleChars(keyE.getText()); // Loại bỏ các kí tự đặc biệt không thể nhìn thấy
 				
-				if (showErrorWhenInputWrong(strKeyTextField, "Key")) {
+				if (showErrorWhenInputWrong(strKeyTextField, "Key")) { // Kiểm tra người dùng nhập key, tránh nhập số và kí tự đặc biệt
 					PlayFair pF = new PlayFair();
 					pF.setKey(strKeyTextField);
 					
 					keyD.setText(pF.xuatArr());
 				}
-				
-				
-				
 			}
 		});
         
@@ -94,35 +86,29 @@ public class playfairScreen extends subScreen {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				// Tạo biến và lưu các giá trị được loại bỏ các kí tự đặc biệt không nhìn thấy được
 				String strPlaintext = removeInvisibleChars(plaintext.getText());
 				String strCipherText = removeInvisibleChars(cipherText.getText());
 				String strKeyTextField = removeInvisibleChars(keyE.getText());
 				
 				if (showErrorWhenInputWrong(strKeyTextField, "Key")) { // Key ko có số và kí tự đặc biệt
 					
-					if ("Encryption".equals((String) choice.getSelectedItem())) {
+					if ("Encryption".equals((String) choice.getSelectedItem())) { // Nếu combobox chọn Encryption
 						
-						if (showErrorWhenInputWrong(strPlaintext, "plaintext")) {
+						if (showErrorWhenInputWrong(strPlaintext, "plaintext")) { // Plaintext không có số và kí tự đặc biệt
 							
 							PlayFair EncryptpF = new PlayFair(strKeyTextField, strPlaintext);
 							cipherText.setText(removeInvisibleChars(EncryptpF.batDauMaHoa()));
 							
 						}
-							
-						
-						
 					}
-					else if ("Decryption".equals((String) choice.getSelectedItem())) {
+					else if ("Decryption".equals((String) choice.getSelectedItem())) { // Nếu combobox chọn Decryption
 						
-						if (showErrorWhenInputWrong(strCipherText, "ciphertext")) {
+						if (showErrorWhenInputWrong(strCipherText, "ciphertext")) { // Ciphertext không có số và kí tự đặc biệt
 							
 							PlayFair DecryptpF = new PlayFair(strKeyTextField, strCipherText);
 							plaintext.setText(removeInvisibleChars(DecryptpF.batDauGiaiMa()));
-							
-							
 						}
-						
-						
 					}
 				}
 				
@@ -133,24 +119,4 @@ public class playfairScreen extends subScreen {
 	    return Panel;
 	}
 	
-
-	
-	private String removeInvisibleChars(String input) {
-        return input.replaceAll("[\\p{C}]+", ""); // Loại bỏ các kí tự không thể nhìn thấy được
-    }
-	
-	private boolean showErrorWhenInputWrong(String str, String name) {
-		boolean check = true;
-		if (str.trim().isEmpty()) {
-    		JOptionPane.showMessageDialog(frame, "Bạn chưa nhập gì ở " + name);
-    		check = false;
-    	}
-    	else if (str.matches(".*[^a-zA-Z ]+.*")) {
-			JOptionPane.showMessageDialog(frame, "Không được nhập số hoặc kí tự đặc biệt ở " + name);
-			check = false;
-		}
-		
-		
-		return check;
-	}
 }
